e6c7748486c650b8e786f75e411b8003f688318c
Mateus Medeiros <mateus.sousamedeiros@gmail.com>
2023-02-01 20:27:40 -0300

    Add: Input, Select and Button components

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,9 +1,21 @@
+import Button from '../../components/Button';
+import Input from '../../components/Input';
 import PageHeader from '../../components/PageHeader';
+import Select from '../../components/Select';
 
 export default function NewContact() {
   return (
-    <PageHeader
-      title="Novo Contato"
-    />
+    <>
+      <PageHeader
+        title="Novo Contato"
+      />
+      <Input placeholder="Nome" />
+
+      <Select>
+        <option value="instagram">Instagram</option>
+      </Select>
+
+      <Button type="button" disabled>Cadastrar</Button>
+    </>
   );
 }
